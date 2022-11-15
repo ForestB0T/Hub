6df86e49853657ccdb1f7d12e0f4fcb1015b3fd6
Febzey <braydent1235@gmail.com>
2022-11-14 22:04:39 -0600

    w

--- src/structure/database/createPool.ts
+++ src/structure/database/createPool.ts
@@ -7,5 +7,4 @@ const database: Pool = createPool(dbConfig);
 database.getConnection(err => err ? console.error(err) : console.log(chalk.greenBright("Connected to database successfully.")));
 
 export const promisedQuery = util.promisify(database.query).bind(database);
-
 export default database; 
