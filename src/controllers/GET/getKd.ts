572c67225200b4c4ebe2042fc0d83b1e722444ae
Febzey <braydent1235@gmail.com>
2023-01-15 17:17:34 -0600

    k

--- src/controllers/GET/getKd.ts
+++ src/controllers/GET/getKd.ts
@@ -11,9 +11,9 @@ export default {
         const serv: string = req.params['server'];
         const user: string = req.params['user'];
         database.Pool.query(`SELECT kills,deaths from users WHERE username = ? AND mc_server = ?`, [user, serv], (err, res) => {
-            if (err || !res[0] || !res[0].kills || !res[0].deaths) return reply.code(501).send({ Error: "user not found." })
-            const kills: number = res[0].kills;
-            const deaths: number = res[0].deaths;
+            if (err || !res[0]) return reply.code(501).send({ Error: "user not found." })
+            const kills: number = res[0].kills || 0;
+            const deaths: number = res[0].deaths || 0;
             reply.code(200).header('Content-Type', 'application/json').send({
                 kills: kills,
                 deaths: deaths
