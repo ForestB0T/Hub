c6b2d0f38f4944b316a173fd20c15068363d54a6
Febzey <braydent1235@gmail.com>
2023-02-24 15:49:25 -0600

    added mcvpg math endpoints.

---
+++ src/controllers/mcvpg/getTopCount.ts
@@ -0,0 +1,19 @@
+import { FastifyReply, FastifyRequest } from "fastify";
+import { RouteItem } from "../../..";
+import { database } from "../../structure/database/createPool";
+
+export default {
+    method: "GET",
+    url: "/mcvpg-math-topsolved",
+    json: true,
+    isPrivate: false,
+    handler: (req: FastifyRequest, reply: FastifyReply, database: database) => {
+        database.Pool.query(`SELECT username,solved_count from mcvpgmath ORDER BY solved_count DESC LIMIT 5`, (err, res) => {
+            if (err || !res) return reply.code(501).send({ Error: "user not found." })
+            reply.code(200).header('Content-Type', 'application/json').send({
+                top_stat: res
+            })
+            return;
+        })
+    }
+} as RouteItem;
---
+++ src/controllers/mcvpg/postMath.ts
@@ -0,0 +1,33 @@
+import { FastifyReply, FastifyRequest } from "fastify";
+import { RouteItem } from "../../..";
+import checkPrivateKey from "../../util/security/keyAuth.js";
+import api from "../../index.js";
+import type { database } from "../../structure/database/createPool";
+
+
+export default {
+    method: "POST", 
+    url: "/mcvpg-save-math/:key",
+    json: true,
+    isPrivate: true,
+    handler: async (req: FastifyRequest, reply: FastifyReply, database: database) => {
+        if (req.params['key'] !== process.env.mcvpgKey) return;
+
+        const user = req.body["username"];
+        const hasSolved = req.body["hasSolved"];
+    
+        try {
+            await database.promisedQuery(
+                "INSERT INTO mcvpgmath (username, solved_count, wrong_count) VALUES (?, ?, ?) ON DUPLICATE KEY UPDATE solved_count = solved_count + ?, wrong_count = wrong_count + ?",
+                [user, hasSolved ? 1 : 0, hasSolved ? 0 : 1, hasSolved ? 1 : 0, hasSolved ? 0 : 1]
+            );
+            reply.send({ success: true });
+        } catch (err) {
+            console.error(err);
+            reply.status(500).send({ success: false, message: 'Internal Server Error' });
+        }
+    
+        return;
+
+    }
+} as RouteItem;
