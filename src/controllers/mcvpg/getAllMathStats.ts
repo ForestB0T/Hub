180741db2ebd9ebec34152b62f29ea6580af9c4e
Febzey <braydent1235@gmail.com>
2023-03-07 18:43:11 -0600

    ww

--- src/controllers/mcvpg/getAllMathStats.ts
+++ src/controllers/mcvpg/getAllMathStats.ts
@@ -12,7 +12,6 @@ export default {
       const username: string = req.params["username"];
       try {
         const stats = await database.promisedQuery(`SELECT solved_count, wrong_count FROM mcvpgmath WHERE username = ?`, [username]);
-        console.log(stats)
         if (stats.length === 0) return reply.code(404).send({ Error: "User not found. "});
         const { solved_count, wrong_count } = stats[0];
 
