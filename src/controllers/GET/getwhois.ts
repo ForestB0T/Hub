a9a089d9407271a0cb95d41262c18f7bfe617230
Febzey <braydent1235@gmail.com>
2023-03-30 00:03:35 -0500

    ww

---
+++ src/controllers/GET/getwhois.ts
@@ -0,0 +1,39 @@
+import { FastifyReply, FastifyRequest } from "fastify";
+import { RouteItem } from "../../..";
+import type { database } from "../../structure/database/createPool";
+
+export default {
+    method: "GET",
+    url: "/whois/:user",
+    json: true,
+    isPrivate: false,
+    handler: async (req: FastifyRequest, reply: FastifyReply, database: database) => {
+
+        const user: string = req.params["user"];
+
+        try {
+            const res = await database.promisedQuery(
+                "SELECT username, description FROM whois WHERE username=?",
+                [user]
+            )
+
+            if (!res || !res.length || !res[0].description) throw new Error("No results.")
+
+            const username = res[0].username;
+            const description = res[0].description;
+            
+            const data = {
+                username: username,
+                description: description
+            };
+            
+            reply.code(200).header("Content-Type", "application/json").send(data);
+
+        } catch (err) {
+            console.error(err);
+            reply.code(200).send({ Error: "Error retrieving from database."})
+            return;
+        }
+
+    }
+} as RouteItem;
---
+++ src/controllers/POST/postIam.ts
@@ -0,0 +1,30 @@
+import { FastifyReply, FastifyRequest } from "fastify";
+import { RouteItem } from "../../..";
+import checkPrivateKey from "../../util/security/keyAuth.js";
+import type { database } from "../../structure/database/createPool";
+
+export default {
+    method: "POST", 
+    url: "/iam/:key",
+    json: true,
+    isPrivate: true,
+    handler: async (req: FastifyRequest, reply: FastifyReply, database: database) => {
+        if (!checkPrivateKey(req.params['key'], reply)) return;
+
+        const user      = req.body["user"], 
+              description = req.body["description"]
+
+        console.log(user, description)
+    
+        try {
+            await database.promisedQuery("INSERT INTO whois (username, description, timestamp) VALUES (?,?,?)", [user, description, Date.now()]);
+            reply.code(200).send({ success: true })
+            return;
+        } catch (err) {
+            console.error(err)
+            reply.code(200).send({ Error: "Database Error while updating user leave." });
+            return;
+     
+        }
+    }
+} as RouteItem;
